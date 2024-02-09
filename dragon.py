2071261b6ce5ccc2891bfd75b5158ef3c0cef8e8
Selenidze <selenich@gmail.com>
2024-02-09 13:58:35 +0300

    Updated dragon.py

--- dragon.py
+++ dragon.py
@@ -4,7 +4,6 @@ import time
 def displayIntro():
     print('''You are in a land full of dragons. In front of you,
 you see two caves. In one cave, the dragon is friendly
-you see two caves. In one cave, the dragon is friendly
 and will share his treasure with you. The other dragon
 is greedy and hungry, and will eat you on sight.''')
     print()
