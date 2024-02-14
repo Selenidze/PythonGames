fc6bed989af26b0fff91921d7d44cdaaf5605190
Selenidze <selenich@gmail.com>
2024-02-14 17:00:53 +0300

    Update remove_unwanted.py and tictactoe.py

--- remove_unwanted/remove_unwanted.py
+++ remove_unwanted/remove_unwanted.py
@@ -14,8 +14,11 @@ TEXTTOREPLACE= [
 ]
 
 def delSequence (listOfStrings, textToSearch, textToReplace):
-    listOfStrings = [re.sub(textToSearch, textToReplace, line) for line in listOfStrings]  
-    return listOfStrings
+    listIndex = 0
+
+    for line in listOfStrings:
+        listOfStrings[listIndex] = re.sub(textToSearch, textToReplace, line)
+        listIndex += 1
 
 def joinStrings(listOfStrings, textToSearch, textToReplace):
     listIndex = len(listOfStrings) - 1
@@ -25,8 +28,6 @@ def joinStrings(listOfStrings, textToSearch, textToReplace):
             listOfStrings[listIndex-1 : listIndex+1] = [textToReplace.join(listOfStrings[listIndex-1 : listIndex+1])]
         listIndex -= 1
 
-    return listOfStrings
-
 def main(textToSearch, textToReplace):
     print("File to perform Search-Replace on:")
     fileToChange = input("--> ")
@@ -34,15 +35,16 @@ def main(textToSearch, textToReplace):
     with open(fileToChange) as fileTR:
         listOfStrings = fileTR.read().splitlines()
 
-    listOfStrings = delSequence(listOfStrings, textToSearch[0], textToReplace[0])       # Removing spaces at the beginning of lines
-    listOfStrings = joinStrings(listOfStrings, textToSearch[1], textToReplace[1])       # Joining strings not starting with a digit 
-    listOfStrings = delSequence(listOfStrings, textToSearch[2], textToReplace[0])       # Removing numbers of lines
+    delSequence(listOfStrings, textToSearch[0], textToReplace[0])                       # Removing spaces at the beginning of lines
+    joinStrings(listOfStrings, textToSearch[1], textToReplace[1])                       # Joining strings not starting with a digit 
+    delSequence(listOfStrings, textToSearch[2], textToReplace[0])                       # Removing numbers of lines
 
     #print(*listOfStrings, sep = "\n")                                                  # Printing the list
 
     with open(fileToChange, 'w') as fileTW:
-        for line in listOfStrings:
-           fileTW.write('%s\n' % line)                                                  # Write each item on a new line
+        for line in listOfStrings[:-1]:
+           fileTW.write('%s\n' % line)                                                  # Write each item on a new line and add \n at the end
+        fileTW.write('%s' % listOfStrings[-1])                                          # Don't add \n at the end of the last line
 
     input('\n\nPress Enter to exit...')
 
